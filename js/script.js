7bd223b1634181e24fe00df56888554b3232f368
mekkiouertani <mekki.ouertani@gmail.com>
2023-10-11 17:02:06 +0200

    scaffolding chessboard

--- js/script.js
+++ js/script.js
@@ -1,6 +1,20 @@
 const cBoardEl = document.getElementById('chessboard'); // prendiamo la scatola della scacchiera
 
+for (let i = 1; i <= 100; i++){
+    const squareEl = document.createElement('div');
+    squareEl.className = `square d-flex justify-content-center align-items-center`;
+    squareEl.innerHTML = i ;
+    cBoardEl.append(squareEl);          
+    
+  
 
+
+
+    //console LOG dentro for
+    console.log(i);
+    console.log(squareEl);
+}
+ 
 
 
 
