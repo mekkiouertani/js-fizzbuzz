90b6b207318d686dccafec3cb9fd299c549b58ae
mekkiouertani <mekki.ouertani@gmail.com>
2023-10-11 18:22:36 +0200

    updated classList to conditions, updated color

--- js/script.js
+++ js/script.js
@@ -1,40 +1,40 @@
-             // prendiamo la scatola della scacchiera
+        // prendiamo la scatola della scacchiera
 const cBoardEl = document.getElementById('chessboard');
-             //dichiaro la variabile bgBox
+        //dichiaro le variabili
 let bgBox;
-             //creo un ciclo FOR
-for (let i = 1; i <= 100; i++){
+const fizz = 'fizz';
+const buzz = 'buzz';
+const fizzBuzz = 'fizzbuzz';
+        //creo un ciclo FOR
+for (let i = 1; i <= 99; i++){
                  //creo un div  
     const squareEl = document.createElement('div'); 
                 //assegno delle classi al div
-    squareEl.className = ` my-aqua ${bgBox} square d-flex justify-content-center align-items-center`;
+    squareEl.className = ` ${bgBox} my-aqua square d-flex justify-content-center align-items-center`;
                 //stampo "i" dentro ai div
     squareEl.innerHTML = i ;
                 //metto il div dentro il div padre 'chessboard'
     cBoardEl.append(squareEl);          
                 //primo IF (buzz) multiplo di 3
-    if (i % 3 > 1){
-        bgBox = 'my-green';
-    }   else{
-        bgBox = 'my-aqua';
-    }           //secondo IF (fizz) multiplo di 5
-    if (i % 5 > 3){
-        bgBox = 'my-yellow';
+    if (i % 3 === 0){
+        squareEl.innerHTML = fizz;
+        squareEl.classList.add('my-green');
+    }   
+               //secondo IF (fizz) multiplo di 5
+    if (i % 5 === 0){
+        squareEl.innerHTML = buzz;
+        squareEl.classList.add('my-yellow');
     }           //terzo IF (buzzfizz) multiplo di 15
-    if  (i % 15 === 14 ){
-        bgBox = 'my-red';
+    if  (i % 3 === 0 && i % 5 === 0){
+        squareEl.innerHTML = fizzBuzz;
+        squareEl.classList.add('my-red');
     } 
   
-
-
-
-    //console LOG dentro for
+    //console LOG 
     console.log(i);
     console.log(squareEl);
+    console.dir(cBoardEl);
 }
 
 
 
-
-//console LOG
-console.dir(cBoardEl);
